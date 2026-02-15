d8a764ab34d3a272ccb3ce5023c57b08a16c138a
mursaleen-Air <mursaleen231213@example.com>
2026-02-15 12:24:10 +0500

    Added Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,17 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                {/* Google Tag Manager */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KK2PH9GK');`,
+                    }}
+                />
+                {/* End Google Tag Manager */}
                 <script
                     async
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1985129550910366"
@@ -44,6 +55,16 @@ export default function RootLayout({
                 ></script>
             </head>
             <body className={`${inter.className} flex flex-col min-h-screen`}>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-KK2PH9GK"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
                 <StructuredData data={generateSoftwareApplicationSchema()} />
                 <AdProvider>
                     <ErrorBoundary>
